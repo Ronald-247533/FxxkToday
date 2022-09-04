82dc2dd598b4f5dcf209303765e6ed177d75154a
Ronald_247533 <57500568+Ronald-247533@users.noreply.github.com>
2022-09-04 19:49:11 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -1,11 +1,10 @@
-from tencentcloud.common.profile.http_profile import HttpProfile
 from actions.wiseLoginService import wiseLoginService
-from actions.autoSign import AutoSign
 from actions.collection import Collection
 from actions.pushKit import pushKit
 from actions.Utils import Utils
 from time import sleep
 import random
+
 
 def main():
     Utils.log("自动化任务开始执行")
@@ -13,10 +12,11 @@ def main():
     push = pushKit(config['notifyOption'])
     httpProxy = config['httpProxy'] if 'httpProxy' in config else ''
     for user in config['users']:
+        times = random.randint(0, 9)
         Utils.log(
-            f"4s后开始执行用户{user['user']['username'] if user['user']['username'] else '默认用户'}的任务"
+            f"{times}s后开始执行用户{user['user']['username'] if user['user']['username'] else '默认用户'}的任务"
         )
-        sleep(random.randint(0,9))
+        sleep(times)
         if config['debug']:
             msg = working(user, httpProxy)
         else:
@@ -28,22 +28,23 @@ def main():
                 ret = False
             ntm = Utils.getTimeStr()
             if ret == True:
-                #此处需要注意就算提示成功也不一定是真的成功，以实际为准
+                # 此处需要注意就算提示成功也不一定是真的成功，以实际为准
                 Utils.log(msg)
                 if 'SUCCESS' in msg:
                     msg = push.sendMsg(
                         '签到成功通知',
-                        '%s，服务器(V%s)于%s尝试签到成功!' % (user['user']['username'],config['Version'], ntm),
+                        '%s，服务器(V%s)于%s尝试签到成功!' % (
+                            user['user']['username'], config['Version'], ntm),
                         user['user'])
                 else:
                     msg = push.sendMsg(
                         '签到异常通知', '%s，服务器(V%s)于%s尝试签到异常!\n异常信息:%s' %
-                        (user['user']['username'],config['Version'], ntm, msg), user['user'])
+                        (user['user']['username'], config['Version'], ntm, msg), user['user'])
             else:
                 Utils.log("Error:" + msg)
                 msg = push.sendMsg(
                     '签到失败通知', '%s，服务器(V%s)于%s尝试签到失败!\n错误信息:%s' %
-                    (user['user']['username'],config['Version'], ntm, msg), user['user'])
+                    (user['user']['username'], config['Version'], ntm, msg), user['user'])
             Utils.log(msg)
     Utils.log("自动化任务执行完毕")
 
@@ -54,38 +55,15 @@ def working(user, httpProxy):
     Utils.log('开始尝试登录账号')
     wise.login()
     sleep(1)
-    # 登陆成功，通过type判断当前属于 信息收集、签到
-    # 信息收集
-    if user['user']['type'] == 0:
-        # 以下代码是信息收集的代码
-        Utils.log('开始执行收集任务')
-        collection = Collection(wise, user['user'])
-        collection.queryForm()
-        collection.fillForm()
-        sleep(1)
-        msg = collection.submitForm()
-        return msg
-    elif user['user']['type'] == 1:
-        # 以下代码是签到的代码
-        Utils.log('开始执行签到任务')
-        sign = AutoSign(wise, user['user'])
-        sign.getUnSignTask()
-        sleep(1)
-        sign.getDetailTask()
-        sign.fillForm()
-        sleep(1)
-        msg = sign.submitForm()
-        return msg
-
-# 阿里云的入口函数
-def handler(event, context):
-    main()
-
-
-# 腾讯云的入口函数
-def main_handler(event, context):
-    main()
-    return 'Finished'
+    # 登陆成功
+    # 信息收集的代码
+    Utils.log('开始执行收集任务')
+    collection = Collection(wise, user['user'])
+    collection.queryForm()
+    collection.fillForm()
+    sleep(1)
+    msg = collection.submitForm()
+    return msg
 
 
 if __name__ == '__main__':
